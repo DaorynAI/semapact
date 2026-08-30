68898ebaf350449f4b7a82531c6a122dc8ab465a
Elliot Sun <elros1109@gmail.com>
2026-08-30 16:23:36 +1000

    feat(databricks): expose discovery workflow

--- semapact/platforms/databricks/__init__.py
+++ semapact/platforms/databricks/__init__.py
@@ -1,5 +1,9 @@
 """Databricks platform-access helpers."""
 
 from semapact.platforms.databricks.client import create_databricks_workspace_client
+from semapact.platforms.databricks.discovery import discover_databricks_tables
 
-__all__ = ["create_databricks_workspace_client"]
+__all__ = [
+    "create_databricks_workspace_client",
+    "discover_databricks_tables",
+]
