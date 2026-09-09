c76d68ca3562bfb4c4e0942d6e90f4a8c2a0d3db
Elliot Sun <elros1109@gmail.com>
2026-09-09 13:26:17 +1000

    feat(contractops): export release planning API

--- semapact/contractops/__init__.py
+++ semapact/contractops/__init__.py
@@ -1,13 +1,17 @@
-"""M2 ContractOps proposal domain."""
+"""M2 ContractOps domain."""
 
 from semapact.contractops.changeset import (
     build_change_set,
     build_change_set_from_decision,
 )
-from semapact.contractops.models import ChangeSet
+from semapact.contractops.models import ChangeSet, ReleasePlan, ReleasePrecondition
+from semapact.contractops.release_plan import build_release_plan
 
 __all__ = [
     "ChangeSet",
+    "ReleasePlan",
+    "ReleasePrecondition",
     "build_change_set",
     "build_change_set_from_decision",
+    "build_release_plan",
 ]
