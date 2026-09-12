c9b3a071b62ca5b5fb786e0e10d889bd14e74180
Elliot Sun <elros1109@gmail.com>
2026-09-12 13:48:25 +1000

    refactor(history): narrow dependency to revision models

--- semapact/history/repository.py
+++ semapact/history/repository.py
@@ -6,7 +6,7 @@ from typing import Protocol
 
 from semapact.contractops import ChangeSet
 from semapact.governance import GovernanceDecision
-from semapact.revision import ContractRevision, ContractRevisionSource
+from semapact.revision.models import ContractRevision, ContractRevisionSource
 
 
 class HistoryRepositoryError(RuntimeError):
