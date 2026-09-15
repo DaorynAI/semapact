48caca3418c4503a00b9795b4da3df17554e9d88
Elliot Sun <elros1109@gmail.com>
2026-09-16 08:42:08 +1000

    feat(sdk): expose approval recording API

--- semapact/__init__.py
+++ semapact/__init__.py
@@ -10,6 +10,8 @@ from importlib import import_module
 from typing import Any
 
 _EXPORTS: dict[str, tuple[str, str]] = {
+    "ApprovalRecord": ("semapact.approval", "ApprovalRecord"),
+    "ApprovalService": ("semapact.application.services.approval", "ApprovalService"),
     "ContractLoader": ("semapact.core.loader", "ContractLoader"),
     "load_contract": ("semapact.core.loader", "load_contract"),
     "ContractValidator": ("semapact.core.validator", "ContractValidator"),
