311048102c929bbb4f450a5360f3a7d096186b7b
Elliot Sun <elros1109@gmail.com>
2026-09-21 14:02:49 +1000

    refactor: remove obsolete compatibility workflow

--- semapact/services/reconciliation_service.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility imports for the former reconciliation service module."""
-
-from semapact.application.models.reconciliation import RuntimeReconciliation
-from semapact.application.services.reconciliation import ReconciliationService
-
-__all__ = ["ReconciliationService", "RuntimeReconciliation"]
