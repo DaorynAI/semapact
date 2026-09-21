445e935a4dd2ef54701a0e273ac7637182f5b9de
Elliot Sun <elros1109@gmail.com>
2026-09-21 14:02:52 +1000

    refactor: remove obsolete compatibility workflow

--- semapact/services/release_models.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility import for the former release application-model module."""
-
-from semapact.application.models.release import ReleasePlanningResult
-
-__all__ = ["ReleasePlanningResult"]
