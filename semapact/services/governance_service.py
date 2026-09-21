3da9b4a89bc2f5ed288fe611b3f4e5152c67f759
Elliot Sun <elros1109@gmail.com>
2026-09-21 14:02:47 +1000

    refactor: remove obsolete compatibility workflow

--- semapact/services/governance_service.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility imports for the former governance service module."""
-
-from semapact.application.models.governance import GovernanceAnalysis, GovernanceProposal
-from semapact.application.services.governance import GovernanceService
-
-__all__ = ["GovernanceAnalysis", "GovernanceProposal", "GovernanceService"]
